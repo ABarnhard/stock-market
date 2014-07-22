ae7cdf375a2196f64c7f44e79d2b3d9019422665
Adam Barnhard <admbarn@gmail.com>
2014-07-21 20:27:45 -0500

    added Client.purchase

--- app/models/client.js
+++ app/models/client.js
@@ -1,10 +1,46 @@
 'use strict';
 
+var Portfolio = require('./portfolio');
+var Stock = require('./stock');
+
 function Client(name, cash){
   this.name = name;
   this.cash = parseFloat(cash);
-  this.portfolios = [];
-  
+  this.portfolio = new Portfolio(name + '\'s portfolio');
+}
+
+Client.prototype.purchase = function(symbol, qty, callback){
+  var that = this;
+  Stock.getQuote(symbol, function(quote){
+    if(that.cash >= quote * qty){
+      that.cash -= quote * qty;
+      that.portfolio.add(symbol, qty);
+      var index = findStock(that.portfolio.stocks, symbol);
+      that.portfolio.stocks[index].price = quote;
+      callback(that);
+    }
+  });
+};
+
+Client.prototype.position = function(){
+  var sum = 0;
+  for(var i = 0; i < this.portfolio.stocks.length; i++){
+    sum += this.portfolio.stocks[i].count * this.portfolio.stocks[i].price;
+  }
+
+  return sum;
+};
+
+// Private helper methods //
+
+function findStock(stocks, symbol){
+  for(var i = 0; i < stocks.length; i++){
+    if(stocks[i].symbol === symbol.toUpperCase()){
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 module.exports = Client;
--- test/unit/client.js
+++ test/unit/client.js
@@ -4,6 +4,7 @@
 
 var expect = require('chai').expect;
 var Client = require('../../app/models/client');
+//var Stock = require('../../app/models/stock');
 
 describe('Client', function(){
   describe('constructor', function(){
@@ -12,7 +13,25 @@ describe('Client', function(){
 
       expect(bob).to.be.instanceof(Client);
       expect(bob.cash).to.equal(10000);
-      expect(bob.portfolios).to.have.length(0);
+      expect(bob.portfolio.stocks).to.have.length(0);
     });
   });
+  describe('#purchase', function(){
+    it('should decrease cash and add stock to position', function(done){
+     var bob = new Client('Bob Smith', 10000);
+
+     bob.purchase('aapl', 50, function(client){
+      expect(client.cash).to.be.below(10000);
+      expect(client.portfolio.stocks.length).to.be.above(0);
+      expect(client.position()).to.be.above(0);
+     });
+     bob.purchase('msft', 25, function(client){
+      expect(client.cash).to.be.below(10000);
+      expect(client.portfolio.stocks.length).to.be.above(0);
+      expect(client.position()).to.be.above(0);
+      done();
+     });
+    });
+  });
+
 });
